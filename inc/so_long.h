7a40af5da2914ffc9dafcf628eb2d89aa335195d
Teresa Bulungu Lufuluabo Pastor <telufulu@c1r7s6.42madrid.com>
2024-02-09 20:09:01 +0100

    working...

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,11 +6,31 @@
 /*   By: telufulu <telufulu@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 18:50:23 by telufulu          #+#    #+#             */
-/*   Updated: 2024/02/09 18:50:52 by telufulu         ###   ########.fr       */
+/*   Updated: 2024/02/09 20:04:16 by telufulu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# include "libft.h"
+# include "mlx.h"
+
+# include <unistd.h>
+# include <fcntl.h>
+
+/*
+ * Structs
+ */
+
+typedef struct	s_map
+{
+	char	**map;
+	int		width;
+	int		height;
+	int		h_y;
+	int		h_x;
+	int		e_y;
+	int		e_x;
+}				t_map;
 #endif
